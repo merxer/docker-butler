641a643d68217f83b2f80538c13673b975be790f
THAWATCHAI SINGNGAM <merxer.io@gmail.com>
2018-10-13 23:39:34 +0700

    delete unused docker images

--- main.go
+++ main.go
@@ -48,6 +48,13 @@ func main() {
 		unusedImages = Filter(unusedImages, container.Image)
 
 	}
-	fmt.Println("# unused images")
-	fmt.Printf("%+v\n", unusedImages)
+
+	for _, removeImage := range unusedImages {
+		ok, err := cli.ImageRemove(context.Background(),
+		                           removeImage,types.ImageRemoveOptions{Force: true, PruneChildren: true})
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v\n", ok)
+	}
 }
